fdf5140c99b0837830867bef949396c15ddb8e0c
Sigurd Hjerde <43098627+sigurdhjerde@users.noreply.github.com>
2019-11-01 12:56:45 +0100

    Added a new learning function and a new deep network

--- diabetes_test2.py
+++ diabetes_test2.py
@@ -48,7 +48,8 @@ db_test = gym.make('HovorkaCambridge-v0')
 init_basal_optimal = 6.43
 eps_init = 1.0
 eps_end = 0.01
-eps_decay = 0.995
+eps_decay = 0.9955
+#epsilon = 0.1
 gamma = 0.99
 sigma = 1e-3
 
@@ -56,7 +57,7 @@ reward = []
 bg = []
 cgm = []
 
-max_episodes = 800
+max_episodes = 1000
 max_steps = 72
 buffer_size = 10000
 batch_size = 32
@@ -143,20 +144,38 @@ class DQN(nn.Module):
             return self.layer(x)
       
 
+class DeepNet(nn.Module):
+      def __init__(self, state_space, action_space):
+            super(DeepNet, self).__init__()
+            
+            self.dense_layer = nn.Sequential(
+                        nn.Linear(state_space, 100),
+                        nn.Tanh(),
+                        nn.Linear(100, 50),
+                        nn.Tanh(),
+                        nn.Linear(50, 25),
+                        nn.Tanh(),
+                        nn.Linear(25, action_space)
+                        )
+      
+      def forward(self, x):
+            return self.dense_layer(x)
+      
+
 class OracleQ(nn.Module):
       def __init__(self, state_space, action_sapce):
             super(OracleQ, self).__init__()
             
             self.fc1 = nn.Sequential(
                         nn.Linear(state_space,256),
-                        nn.BatchNorm1d(256),
-                        nn.ReLU()
+                        #nn.BatchNorm1d(256),
+                        nn.Tanh()
                         )
             
             self.fc2 = nn.Sequential(
                         nn.Linear(256, action_space),
-                        nn.BatchNorm1d(action_space),
-                        nn.ReLU()
+                        #nn.BatchNorm1d(action_space),
+                        nn.Tanh()
                         )
             
       def forward(self, x):
@@ -166,8 +185,11 @@ class OracleQ(nn.Module):
 
 #%%
 
-model = DQN(state_space, action_space)
-model_target = DQN(state_space, action_space)
+#model = DQN(state_space, action_space)
+#model_target = DQN(state_space, action_space)
+
+model = OracleQ(state_space, action_space)
+model_target = OracleQ(state_space, action_space)
 
 loss = nn.MSELoss()
 optimizer = optim.Adam(params = model.parameters(), lr = learning_rate)
@@ -187,25 +209,39 @@ def choose_action(Q, epsilon):
       
 # Learning function. Returns loss 
 def learn(batch_size, gamma):
+      # Checks if buffer has enough samples.
+      # If not, then return and continue gathering samples.
       if buffer.size() < batch_size:
             return
       
+      # Sample transitions to be used during training
       cs_batch, a_batch, r_batch, d_batch, ns_batch = buffer.sample(batch_size)
       
+      # Convert current and next states samples into torch tensors
+      cs_batch = th.from_numpy(cs_batch).float()
+      ns_batch = th.from_numpy(ns_batch).float()
+      
+      # Initialize target Q-values
       Q_target = th.zeros((batch_size, action_space))
       
+      # Loop through the batches to learn
       for b in range(batch_size):
-            Q_new = model_target(th.from_numpy(ns_batch[b]).float())
+            # Q-values from target network based on next state
+            Q_new = model_target(ns_batch[b])
+            # Maximum Q-value of Q_new
             maxQ_new = th.max(Q_new.data)
-      
-            Q_target[b] = model_target(th.from_numpy(cs_batch[b]).float())
+            
+            # Fill target Q-values with samples and update
+            Q_target[b] = model_target(cs_batch[b])
             Q_target[b, a_batch[b]] = r_batch[b]
-      
+            
             if d_batch[b] == False:
                   Q_target[b, a_batch[b]] += gamma * maxQ_new
       
-      Q_batch = model(th.from_numpy(cs_batch).float())
+      # Q-values from current states
+      Q_batch = model(cs_batch)
       
+      # Get the loss
       train_loss = loss(Q_batch, Q_target)
       
       # Minimize the loss and backpropagate
@@ -215,45 +251,65 @@ def learn(batch_size, gamma):
       
       return train_loss
 
+# New learning. Can run either Q-learning or DQ-learning procedure
 def learn_new(batch_size, gamma, DDQ_learning):
+      # Checks if buffer has enough samples.
+      # If not, then return and continue gathering samples.
       if buffer.size() < batch_size:
             return
       
+      # Sample transitions to be used during training
       cs_batch, a_batch, r_batch, d_batch, ns_batch = buffer.sample(batch_size)
       
+      # Convert current and next states samples into torch tensors
       cs_batch = th.from_numpy(cs_batch).float()
       ns_batch = th.from_numpy(ns_batch).float()
       
+      # Initialize target Q-values
       Q_target = th.zeros((batch_size, action_space))
       
+      # Check whether or not to run DQ-learning procedure
+      # Q-learning
       if not DDQ_learning:
+            # Loop through the batches to learn
             for b in range(batch_size):
+                  # Q-values from target network based on next state
                   Q_new = model_target(ns_batch[b])
+                  # Maximum Q-value of Q_new
                   maxQ_new = th.max(Q_new.data)
-            
+                  
+                  # Fill target Q-values with samples and update
                   Q_target[b] = model_target(cs_batch[b])
                   Q_target[b, a_batch[b]] = r_batch[b]
             
                   if d_batch[b] == False:
                         Q_target[b, a_batch[b]] += gamma * maxQ_new
-            
+      # DQ-learning      
       else:
+            # Loop through the batches to learn
             for b in range(batch_size):
+                  # Q-values from local network based on next state
                   Q_new = model(ns_batch[b])
+                  # Pick the best action from Q_new
                   action_best = np.argmax(Q_new.data.numpy())
-            
+                  
+                  # Q-values from target network based on next state
                   Q_target_next = model_target(ns_batch[b])
-            
+                  
+                  # Fill target Q-values with samples and update
                   Q_target[b] = model_target(cs_batch[b])
                   Q_target[b, a_batch[b]] = r_batch[b]
             
                   if d_batch[b] == False:
                         Q_target[b, a_batch[b]] += gamma * Q_target_next[action_best]
       
+      # Q-values from curren states
       Q_batch = model(cs_batch)
       
+      # Get the loss
       train_loss = loss(Q_batch, Q_target)
       
+      # Minimize the loss and backpropagate
       optimizer.zero_grad()
       train_loss.backward()
       optimizer.step()
@@ -285,6 +341,7 @@ def train(episodes, steps, epsilon, DDQ_learning):
             
             # Reward sum, loss sum and time steps reset
             reward_all = 0
+            r = []
             t = 0
             
             # Time step loop
@@ -304,6 +361,7 @@ def train(episodes, steps, epsilon, DDQ_learning):
                   
                   # Reward sum
                   reward_all += reward
+                  r.append(reward)
                   # Updating current state
                   state = next_state
                   # Incrementing time steps
@@ -327,11 +385,14 @@ def train(episodes, steps, epsilon, DDQ_learning):
             
             if epi % 100 == 0:
                   print('Episode: {}'.format(epi))
+            
+            if epi % 10 == 0:
+                  print('Average reward: {}'.format(np.mean(r)))
                   
 
 #%%
 
-train(max_episodes, max_steps, eps_init, True)
+train(max_episodes, max_steps, eps_init, False)
 
 
 #%%
@@ -378,6 +439,7 @@ state = db_test.reset()
 model.eval()
 for i in range(72):
       q = model(th.from_numpy(state).float())
+      print(q)
       a = choose_action(q,0)
       action_list.append(a)
       
@@ -508,6 +570,7 @@ def train(episodes, steps, batch_size, epsilon):
             
             # Reward sum, loss sum and time steps reset
             reward_all = 0
+            r = []
             t = 0
             
             # Time step loop
@@ -530,6 +593,7 @@ def train(episodes, steps, batch_size, epsilon):
                   
                   # Reward sum
                   reward_all += reward
+                  r.append(reward)
                   # Updating current state
                   state = next_state
                   # Incrementing time steps
@@ -540,7 +604,7 @@ def train(episodes, steps, batch_size, epsilon):
                         break
             
             # Update epsilon
-            epsilon = max(eps_end, eps_decay * epsilon)
+            #epsilon = max(eps_end, eps_decay * epsilon)
             
             # Update target network
             target_update(model, model_target, sigma)
@@ -555,7 +619,7 @@ def train(episodes, steps, batch_size, epsilon):
                   print('Episode: {}'.format(epi))
             
             if epi % 10 == 0:
-                  print('Average reward: {}'.format(np.mean(reward_list)))
+                  print('Average reward: {}'.format(np.mean(r)))
 
 #%%
 #-------------------------
@@ -573,7 +637,7 @@ while buffer.size() < buffer_size:
 
 #%%
       
-train(max_episodes, max_steps, batch_size, eps_init)
+train(max_episodes, max_steps, batch_size, epsilon)
 
 
 
@@ -587,6 +651,7 @@ model.eval()
 for i in range(72):
       q = model(th.from_numpy(np.atleast_2d(state)).float())
       a = choose_action(q,0)
+      print(q)
       action_list.append(a)
       
       s,r,d,i = db_test.step(a)
